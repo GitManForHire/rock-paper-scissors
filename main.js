210d51b05c247087e1fad9e19314e7f71fada023
Agent 84 <gitmanforhire@tutanota.com>
2021-12-12 23:34:26 +0000

    Update with a simple UI

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+const rockBtn = document.getElementById("rock");
+const paperBtn = document.getElementById("paper");
+const scissorsBtn = document.getElementById("scissors");
+const results = document.getElementById("results");
+const playerScore = document.getElementById("player-score");
+const computerScore = document.getElementById("computer-score");
+
+playerScore.innerText = "0";
+computerScore.innerText = "0";
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", btnClick.bind(this, button.value));
+});
+
+function btnClick(btnValue) {
+  playRound(btnValue, computerPlay());
+}
+
 // Randomly generate computer's play
 function computerPlay() {
   let computerChoice;
@@ -15,17 +35,6 @@ function computerPlay() {
   return computerChoice;
 }
 
-// Prompt player for input
-function getPlayerInput() {
-  let playerChoice = prompt("Please enter your choice (rock|paper|scissors): ");
-  playerChoice = playerChoice.toLowerCase();
-  while (playerChoice !== "rock" && playerChoice !== "paper" && playerChoice !== "scissors") {
-    playerChoice = prompt("Invalid choice - please try again (rock|paper|scissors): ");
-    playerChoice = playerChoice.toLowerCase();
-  }
-  return playerChoice;
-}
-
 // Capitalize a word
 function capitalizeWord(word) {
   word = word.toLowerCase();
@@ -37,40 +46,72 @@ function capitalizeWord(word) {
 
 // Determine the winner and the loser of the round
 function playRound(playerSelection, computerSelection) {
+  let result = "";
+  let userScore = Number(playerScore.innerText);
+  let pcScore = Number(computerScore.innerText);
+  
   if (playerSelection === computerSelection) {
-    return `It's a draw! ${capitalizeWord(playerSelection)} ties with ${computerSelection}.`;
+    result = `It's a draw! 😐 ${capitalizeWord(playerSelection)} ties with ${computerSelection}.`;
   }
   else if (playerSelection === "rock") {
     if (computerSelection === "paper") {
-      return `You lose! ${capitalizeWord(computerSelection)} beats ${playerSelection}.`;
+      result = `You lose! 🙁 ${capitalizeWord(computerSelection)} beats ${playerSelection}.`;
+      pcScore++;
     }
     else if (computerSelection === "scissors") {
-      return `You win! ${capitalizeWord(playerSelection)} beats ${computerSelection}.`;
+      result = `You win! 🙂 ${capitalizeWord(playerSelection)} beats ${computerSelection}.`;
+      userScore++;
     }
   }
   else if (playerSelection === "paper") {
     if (computerSelection === "rock") {
-      return `You win! ${capitalizeWord(playerSelection)} beats ${computerSelection}.`;
+      result = `You win! 🙂 ${capitalizeWord(playerSelection)} beats ${computerSelection}.`;
+      userScore++;
     }
     else if (computerSelection === "scissors") {
-      return `You lose! ${capitalizeWord(computerSelection)} beats ${playerSelection}.`;
+      result = `You lose! 🙁 ${capitalizeWord(computerSelection)} beats ${playerSelection}.`;
+      pcScore++;
     }
   }
   else if (playerSelection === "scissors") {
     if (computerSelection === "rock") {
-      return `You lose! ${capitalizeWord(computerSelection)} beats ${playerSelection}.`;
+      result = `You lose! 🙁 ${capitalizeWord(computerSelection)} beats ${playerSelection}.`;
+      pcScore++;
     }
     else if (computerSelection === "paper") {
-      return `You win! ${capitalizeWord(playerSelection)} beats ${computerSelection}.`;
+      result = `You win! 🙂 ${capitalizeWord(playerSelection)} beats ${computerSelection}.`;
+      userScore++;
     }
   }
+  
+  updateResults(userScore, pcScore, result);
 }
 
-// Play 5 rounds
-function game() {
-  for (let i = 0; i < 5; i++) {
-    console.log(playRound(getPlayerInput(), computerPlay()));
+// Update page with results of the round
+function updateResults(userScore, pcScore, result) {
+  playerScore.innerText = userScore.toString();
+  computerScore.innerText = pcScore.toString();
+  
+  // Check to see if the player or computer has scored 5 points yet
+  if (userScore === 5) {
+    displayWin(true);
+  }
+  else if (pcScore === 5) {
+    displayWin(false);
+  }
+  else {
+    let outcome = document.createElement("p");
+    outcome.textContent = result;
+    results.appendChild(outcome);
   }
 }
 
-game();
+// Disable the buttons and display a win or loss message to the player
+function displayWin(playerWon) {
+  buttons.forEach((button) => {
+    button.disabled = true;
+  });
+  
+  results.textContent = playerWon ? "Congratulations! 🥳 You won this game!" : "Too bad! 🤖 The computer won this game!";
+  results.style.fontSize = "25px";
+}
